c11f780249064a6ff2c870ef10d38fd2d7034716
Michael Harris <mhguitar1997@gmail.com>
2019-07-09 15:52:15 -0700

    Button adds number to array

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
-  const [numbers, setNumbers] = useState([0])
+  const [numbers, setNumbers] = useState([])
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
-  const addNumber = num => setNumbers(numbers.push(num))
+  const addNumber = num => setNumbers([...numbers, num])
 
   return (
     <div className="container">
