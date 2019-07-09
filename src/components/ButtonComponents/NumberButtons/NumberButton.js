64098f11178102ee93d1dce7046d1cc0ca19350b
Michael Harris <mhguitar1997@gmail.com>
2019-07-09 15:38:07 -0700

    Pass addNumber function to button and on Click add

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -6,7 +6,7 @@ const NumberButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className={styles.numbers}>{props.number}</button>
+      <button className={styles.numbers} onClick={() => props.addNumber(props.number)}>{props.number}</button>
     </>
   );
 };
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -2,17 +2,16 @@ import React, {useState} from "react";
 import {numbers as nums} from '../../../data'
 import NumberButton from "./NumberButton";
 
-import styles from '../../../calculator.module.css';
 
 
-const Numbers = () => {
+const Numbers = (props) => {
   const [numbers, setNumber] = useState(nums)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {numbers.map(num => <NumberButton number={num}/>)}
+       {numbers.map(num => <NumberButton number={num} addNumber={props.addNumber}/>)}
     </div>
   );
 };
