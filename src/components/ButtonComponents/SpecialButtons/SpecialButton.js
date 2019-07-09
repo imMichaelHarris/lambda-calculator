6d4d6fcd2f474afead51c56cc58a014154aff8bd
Michael Harris <mhguitar1997@gmail.com>
2019-07-09 13:35:21 -0700

    Export Specails and special button

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -3,7 +3,8 @@ import React from "react";
 const SpecialButton = () => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
+
+export default SpecialButton;
