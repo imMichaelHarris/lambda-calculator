92758b84273887497826bc6da93f652eaeb5af32
Michael Harris <mhguitar1997@gmail.com>
2019-07-09 12:37:55 -0700

    Create button element that renders number props

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -10,7 +10,7 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {numbers.map(num => <NumberButton num={num}/>)}
+       {numbers.map(num => <NumberButton number={num}/>)}
     </div>
   );
 };
