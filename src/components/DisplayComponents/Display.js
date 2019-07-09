409f2209b2ecda6285ff766ecc6ef0061dff27c7
Michael Harris <mhguitar1997@gmail.com>
2019-07-09 13:06:47 -0700

    Render display in App.js

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Display = () => {
   return <div>{/* Display any props data here */}
-    <input type="number">0</input>
+    <input type="number" placeholder="0"/>
   </div>;
 };
 export default Display;
