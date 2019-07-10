411cb8691f50401f2d8ce7ee46968fb448c7cc39
Michael Harris <mhguitar1997@gmail.com>
2019-07-10 00:17:13 -0700

    Create expression state

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   const [numbers, setNumbers] = useState([])
+  const [expression, setExpression] = useState([])
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
