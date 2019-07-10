0fdfcceae501b91208c9853c57bf3a3451b1e11a
Michael Harris <mhguitar1997@gmail.com>
2019-07-10 00:39:30 -0700

    Styling to match design - buttons not in place

--- src/App.js
+++ src/App.js
@@ -12,20 +12,14 @@ import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
-  const [numbers, setNumbers] = useState([])
   const [expression, setExpression] = useState([])
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
-  const addNumber = num => setNumbers([...numbers, num])
   const addExpression = num => setExpression([...expression, num])
 
-  const newOperator = operator => setNumbers({
-    numbers: numbers,
-
-  })
   const clearDisplay = () => setExpression([])
 console.log(expression)
   return (
@@ -36,7 +30,7 @@ console.log(expression)
         <Display expression={expression}/>
         <div>
           <Specials clearDisplay={clearDisplay} />
-          <Numbers addNumber={addNumber} addExpression={addExpression} />
+          <Numbers addExpression={addExpression} />
           <Operators addExpression={addExpression}/>
         </div>
       </div>
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -11,7 +11,7 @@ const Numbers = (props) => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {numbers.map(num => <NumberButton number={num} addNumber={props.addNumber} addExpression={props.addExpression }/>)}
+       {numbers.map(num => <NumberButton number={num}  addExpression={props.addExpression }/>)}
     </div>
   );
 };
